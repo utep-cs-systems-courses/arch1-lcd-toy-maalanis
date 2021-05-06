f91339dbc46372af9febd5ffae4225b42b9a46c3
Marco <maalanis@miners.utep.edu>
2021-05-06 15:53:34 -0600

    cleaned main

--- shapeLib/main.c
+++ shapeLib/main.c
@@ -18,133 +18,6 @@ u_char count = 0;
 AbRect rect10 = {abRectGetBounds, abRectCheck, {10,10}}; /**< 10x10 rectangle */
 AbRArrow rightArrow = {abRArrowGetBounds, abRArrowCheck, 30};
 
-AbRectOutline fieldOutline = {	/* playing field */
-  abRectOutlineGetBounds, abRectOutlineCheck,   
-  {screenWidth/2 - 10, screenHeight/3- 10}
-};
-
-Layer layer4 = {
-  (AbShape *)&rightArrow,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_PINK,
-  0
-};
-  
-
-Layer layer3 = {		/**< Layer with an orange circle */
-  (AbShape *)&circle8,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_VIOLET,
-  &layer4,
-};
-
-
-Layer fieldLayer = {		/* playing field as a layer */
-  (AbShape *) &fieldOutline,
-  {screenWidth/2, screenHeight/2},/**< center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_BLACK,
-  &layer3
-};
-
-Layer layer1 = {		/**< Layer with a red square */
-  (AbShape *)&rect10,
-  {screenWidth/2, screenHeight/2}, /**< center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_RED,
-  &fieldLayer,
-};
-
-Layer layer0 = {		/**< Layer with an orange circle */
-  (AbShape *)&circle14,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_ORANGE,
-  &layer1,
-};
-
-/** Moving Layer
- *  Linked list of layer references
- *  Velocity represents one iteration of change (direction & magnitude)
- */
-typedef struct MovLayer_s {
-  Layer *layer;
-  Vec2 velocity;
-  struct MovLayer_s *next;
-} MovLayer;
-
-/* initial value of {0,0} will be overwritten */
-MovLayer ml3 = { &layer3, {1,1}, 0 }; /**< not all layers move */
-MovLayer ml1 = { &layer1, {1,2}, &ml3 }; 
-MovLayer ml0 = { &layer0, {2,1}, &ml1 }; 
-
-void movLayerDraw(MovLayer *movLayers, Layer *layers)
-{
-  int row, col;
-  MovLayer *movLayer;
-
-  and_sr(~8);			/**< disable interrupts (GIE off) */
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Layer *l = movLayer->layer;
-    l->posLast = l->pos;
-    l->pos = l->posNext;
-  }
-  or_sr(8);			/**< disable interrupts (GIE on) */
-
-
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Region bounds;
-    layerGetBounds(movLayer->layer, &bounds);
-    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
-		bounds.botRight.axes[0], bounds.botRight.axes[1]);
-    for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
-      for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
-	Vec2 pixelPos = {col, row};
-	u_int color = bgColor;
-	Layer *probeLayer;
-	for (probeLayer = layers; probeLayer; 
-	     probeLayer = probeLayer->next) { /* probe all layers, in order */
-	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
-	    color = probeLayer->color;
-	    break; 
-	  } /* if probe check */
-	} // for checking all layers at col, row
-	lcd_writeColor(color); 
-      } // for col
-    } // for row
-  } // for moving layer being updated
-}	  
-
-
-
-//Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
-
-/** Advances a moving shape within a fence
- *  
- *  \param ml The moving shape to be advanced
- *  \param fence The region which will serve as a boundary for ml
- */
-void mlAdvance(MovLayer *ml, Region *fence)
-{
-  Vec2 newPos;
-  u_char axis;
-  Region shapeBoundary;
-  for (; ml; ml = ml->next) {
-    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
-    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
-    for (axis = 0; axis < 2; axis ++) {
-      if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
-	  (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
-	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
-	newPos.axes[axis] += (2*velocity);
-      }	/**< if outside of fence */
-    } /**< for axis */
-    ml->layer->posNext = newPos;
-  } /**< for ml */
-}
-
 
 u_int bgColor = COLOR_BLUE;     /**< The background color */
 int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
@@ -152,38 +25,36 @@ int redrawScreen = 1;           /**< Boolean for whether screen needs to be redr
 Region fieldFence;		/**< fence around playing field  */
 
 
-
+/*
 Circle circle14;
 
 Vec2 circle1Pos, circle2Pos, circle1Velocity, circle2Velocity;
 
 void initCircles() {
   static u_char chords14[15];	/* for a circle of radius 14 */
-  computeChordVec(chords14, 14);
-  circle14.radius = 14;
-  circle14.chords = chords14;
-  for (axis = 0; axis < 2; axis ++) {
-    circle1Pos.axes[axis] = screenCenter.axes[axis] + 5;
-    circle2Pos.axes[axis] = screenCenter.axes[axis] - 5;
-    circle1Velocity[axis] = 1;
-    circle1Velocity[axis] = 2;
-  }
-  circle1.color = COLOR_RED;
-  circle2.color = COLOR_ORANGE;
-}
-    
-
+// computeChordVec(chords14, 14);
+//circle14.radius = 14;
+//circle14.chords = chords14;
+//for (axis = 0; axis < 2; axis ++) {
+    //circle1Pos.axes[axis] = screenCenter.axes[axis] + 5;
+    //circle2Pos.axes[axis] = screenCenter.axes[axis] - 5;
+    // circle1Velocity[axis] = 1;
+    //  circle1Velocity[axis] = 2;
+    // }
+// circle1.color = COLOR_RED;
+// circle2.color = COLOR_ORANGE;
+//}
 
 /* return true if pixelPos is within circle at circlePos */
-int
-circleCheck(Vec2 *pixelPos, Circle *circle, Vec2 *circlePos)
-{
-  Vec2 relPos;			/* pixel location, relative to circle center */
-  vec2Sub(&relPos, &pixelPos, &circlePas);
-  s_char row = abs(relPos->axes[0]);
-  s_char col = abs(relPos->axes[1]);
-  return (col <= circle->radius) && (row <= circle->chords[col]);
-}
+//int
+//circleCheck(Vec2 *pixelPos, Circle *circle, Vec2 *circlePos)
+//{
+// Vec2 relPos;			/* pixel location, relative to circle center */
+// vec2Sub(&relPos, &pixelPos, &circlePas);
+// s_char row = abs(relPos->axes[0]);
+// s_char col = abs(relPos->axes[1]);
+// return (col <= circle->radius) && (row <= circle->chords[col]);
+//}
   
 
 void drawArena()
@@ -191,16 +62,16 @@ void drawArena()
   drawRectOutline(LEFT_BOUNDARY, LOWER_BOUNDARY, ARENA_WIDTH, ARENA_HEIGHT, COLOR_BLACK);
 }
 
-fillCircle(u_char col, u_char row, u_char radius, u_char chords[], u_int color)
-{
-  u_char drow;
-  for (drow = 0; drow < radius; drow++) {
-    u_char dcol = chords[drow];
-    u_char width = 1 + (dcol << 1);
-    fillRectangle(col-dcol, row+drow, width, 1, color);
-    fillRectangle(col-dcol, row-drow, width, 1, color);
-  }
-}
+//fillCircle(u_char col, u_char row, u_char radius, u_char chords[], u_int color)
+//{
+// u_char drow;
+// for (drow = 0; drow < radius; drow++) {
+//   u_char dcol = chords[drow];
+//   u_char width = 1 + (dcol << 1);
+//   fillRectangle(col-dcol, row+drow, width, 1, color);
+//   fillRectangle(col-dcol, row-drow, width, 1, color);
+// }
+//}
 
 
 main()
@@ -230,10 +101,10 @@ main()
   drawString5x7((width>>1) - 20, 20, "Arena", COLOR_GREEN, COLOR_RED);
   
   drawArena();
-  fillCircle(pcol, prow, RADIUS, chords14, COLOR_RED);
+  //fillCircle(pcol, prow, RADIUS, chords14, COLOR_RED);
   
-  or_sr(0x8);
-  // or_sr(0x18);  /* CPU off, GIE on */
+  //  or_sr(0x8);
+   or_sr(0x18);  /* CPU off, GIE on */
   
   for(;;) { 
     while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
